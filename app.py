8f3872ac95fb8837225c25a92802ae5d0948ae25
cauaedm <cauaedm@dcc.ufrj.br>
2024-09-22 18:31:59 -0300

    adicionando metricas

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import ETL
-
+import plotly.express as px
 
 # read the data
 df = pd.read_csv('data.csv')
@@ -27,7 +27,7 @@ previous_price = df['Preco'][1]  # Supondo que o segundo valor é o anterior
 price_change = latest_price - previous_price
 
 # Exibir o valor do último preço como métrica
-st.metric(label="Último Preço", value=f"R${latest_price:.2f}", delta=f"R${price_change:.2f}")
+st.metric(label=f"Última divulgação {df['Data'][0].split('-')[2]}/{df['Data'][0].split('-')[1]}/{df['Data'][0].split('-')[0]}", value=f"R${latest_price:.2f}", delta=f"R${price_change:.2f}")
 
 # Create Plotly figure
 fig = go.Figure()
@@ -41,4 +41,33 @@ fig.update_layout(title='Time Series Plot',
                   yaxis_title='Value')
 
 # Display the plot in Streamlit
+st.plotly_chart(fig)
+
+col1, col2 = st.columns(2)
+
+# Colocar as métricas dentro das colunas
+with col1:
+    st.metric(label="Média do último ano:", value=f"{np.mean(np.array(df['Preco'][:12])):.2f}", delta=f"{(np.mean(np.array(df['Preco'][:12]) - np.array(df['Preco'][12:24]))):.2f}")
+
+with col2:
+    st.metric(label="ada", value=10)
+
+    
+df["month"] = df['Data'].str.split('-').str[1]
+
+# Agrupar pelo mês e calcular a média dos preços
+mean_prices = df['Preco'].groupby(df['month']).mean().reset_index()
+mean_prices.columns = ['month', 'mean_price']  # Renomear colunas
+
+# Criar o gráfico de barras com Plotly
+fig = px.bar(mean_prices, x='month', y='mean_price', 
+              title='Média de Preços por Mês', 
+              labels={'month': 'Mês', 'mean_price': 'Média de Preços'},
+              color='mean_price')
+
+# Configurar o layout do gráfico
+fig.update_layout(xaxis_title='Mês', yaxis_title='Média de Preços')
+fig.update_yaxes(type='log')
+
+# Exibir o gráfico no Streamlit
 st.plotly_chart(fig)
